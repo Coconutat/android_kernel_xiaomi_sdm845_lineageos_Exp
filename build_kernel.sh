b38bbf269158cdbcb858c25a4382c6ca8b900e09
Coconut <coconutat@protonmail.com>
2025-10-03 15:24:14 +0800

    build script: Switch to SukiSU

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,7 +2,7 @@
 #设置环境
 
 # 交叉编译器路径
-export PATH=$PATH:$(pwd)/../Compiler/ZyC_clang20/bin
+export PATH=$PATH:$(pwd)/../Compiler/ZyC_clang16/bin
 export CC=clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=aarch64-linux-gnu-
