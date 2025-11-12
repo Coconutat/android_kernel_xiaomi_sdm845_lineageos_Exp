012c09cb3680dc79152192db3ef5f7890e8827b7
Coconut <coconutat@protonmail.com>
2025-11-12 19:32:02 +0800

    build: fix string

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,7 +3,7 @@
 
 # 交叉编译器路径
 export PATH=$PATH:$(pwd)/../Compiler/Zyc_Clang_16/bin
-export CC=clang
+CC="ccache clang"
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
@@ -31,11 +31,11 @@ if [ "$version" == "" ]; then
 fi
 case $version in
     [Aa]*)
-		make ARCH=arm64 O=out "ccache clang" vendor/xiaomi/mi845_mod_defconfig vendor/xiaomi/extra.config vendor/xiaomi/ursa.config
+		make ARCH=arm64 O=out CC="ccache clang" vendor/xiaomi/mi845_mod_defconfig vendor/xiaomi/extra.config vendor/xiaomi/ursa.config
 		device_name=ursa
 		;;
 	[Bb]*)
-        make ARCH=arm64 O=out "ccache clang" vendor/xiaomi/mi845_mod_defconfig vendor/xiaomi/extra.config vendor/xiaomi/dipper.config
+        make ARCH=arm64 O=out CC="ccache clang" vendor/xiaomi/mi845_mod_defconfig vendor/xiaomi/extra.config vendor/xiaomi/dipper.config
 		device_name=dipper
     	;;
 	*)
