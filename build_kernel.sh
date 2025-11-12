88166317d64b362ee73aa520516959440f3723e7
Coconut <coconutat@protonmail.com>
2025-11-12 19:28:20 +0800

    fix error

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,7 +3,7 @@
 
 # 交叉编译器路径
 export PATH=$PATH:$(pwd)/../Compiler/Zyc_Clang_16/bin
-export CC="ccache clang"
+export CC=clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
