ed838d39f92166ccb93c4c047569c53a554154d7
Coconut <coconutat@protonmail.com>
2026-04-06 20:00:32 +0800

    AnyKernel3: Add Script

---
+++ tools/AnyKernel3_sh/anykernel.sh
@@ -0,0 +1,36 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+properties() { '
+kernel.string=Xiaomi Mi 8 Explorer Edition Kernel Mod For LineageOS 22+ Version -- Kernel MOD & Compile by Coconutat @ Github
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=ursa
+device.name2=equuleus
+device.name3=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+## boot shell variables
+BLOCK=auto;
+IS_SLOT_DEVICE=auto;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+no_magisk_check=1;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes;
+
+# boot install
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
+    split_boot # for devices with init_boot ramdisk
+    flash_boot # for devices with init_boot ramdisk
+else
+    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+fi
+## end boot install
